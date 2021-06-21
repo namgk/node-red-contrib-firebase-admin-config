d6de08f9b605777a847dc9a6c9c1b9737da828d4
Nam Giang <kyng@ece.ubc.ca>
2021-06-21 16:10:31 -0700

    init

---
+++ src/firebaseAdminNode.js
@@ -0,0 +1,33 @@
+var firebaseAdmin = require('firebase-admin');
+
+function FirebaseAdminNode(config) {
+  if (!config.serviceAccountJson){
+    throw 'Service Account Json Not Present';
+  }
+
+  this.app = firebaseAdmin.initializeApp({
+	  credential: firebaseAdmin.credential.cert(config.serviceAccountJson),
+	  databaseURL: `https://${config.serviceAccountJson.project_id}.firebaseio.com`
+	});
+
+	this._firebaseAdmin = firebaseAdmin;
+	this.messaging = firebaseAdmin.messaging();
+	this.database = firebaseAdmin.database();
+}
+
+FirebaseAdminNode.prototype.onClose = function(removed, done) {
+	let deletePromises = [];
+	firebaseAdmin.apps.forEach((app) => {
+    deletePromises.push(app.delete());
+  });
+  Promise.all(deletePromises)
+  .then(()=>{
+ 		done()
+ 	})
+ 	.catch((e)=>{
+  	console.error(e)
+ 		done()
+ });
+};
+
+module.exports = FirebaseAdminNode
